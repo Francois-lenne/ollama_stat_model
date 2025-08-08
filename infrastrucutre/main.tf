03be036db1a6702991d9d22866729c2e7c91c1bc
François Lenne <114836746+Francois-lenne@users.noreply.github.com>
2025-08-08 10:33:55 +0200

    end of the first code and creation of the infra

---
+++ infrastrucutre/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "storagedemoflenne${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "demo"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+output "container_url" {
+  value = "${azurerm_storage_account.storage.primary_blob_endpoint}${azurerm_storage_container.container.name}"
+}
---
+++ infrastrucutre/variables.tf
@@ -0,0 +1,14 @@
+variable "container_name" {
+  description = "Nom du container Blob"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources"
+  type        = string
+}
+
+variable "location" {
+  description = "Région Azure"
+  type        = string
+}
